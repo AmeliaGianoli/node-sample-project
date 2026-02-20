b90a1f67ececf0d91228c392ff7deb68afe3d1ec
Amelia Gianoli <gianolia@students.westerntc.edu>
2026-02-20 11:42:45 -0600

    added markdown-helpers.js and markdown-helper-tests.js files

---
+++ samples/markdown-helper-tests.js
@@ -0,0 +1,5 @@
+const {convertMarkdown, getMarkdownMetaData}=require("../modules/markdown-helpers");
+
+const filePath=__dirname+"/sample.md"
+console.log(convertMarkdown(filePath));
+console.log(getMarkdownMetaData(filePath))
