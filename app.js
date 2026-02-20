68ea2b6de6d7510e076d29a39dd9f9e1393f5c9a
Amelia Gianoli <gianolia@students.westerntc.edu>
2026-02-20 13:57:40 -0600

    Added public folder and subsequent files (app.js, main.js, main.css, static-page.html

---
+++ app.js
@@ -0,0 +1,34 @@
+// imports:
+const express=require('express');
+const app = express();
+
+// MIDDLEWARE
+// this is a spot for features/functionality
+
+// Set the 'public' folder as the location for our static files:
+app.use(express.static('public'));
+
+
+// ROUTES
+
+// default:
+app.get('/', (req, res) =>{
+  res.send('<h1>Hello World from Express!</h1>');
+});
+
+// host/some-route:
+app.get('/some-route', (req, res)=>{
+  res.send('<h1>This is some route</h1>');
+});
+
+// example of a dynamic ROUTE:
+app.get("/dynamic-page.html", (req, res)=>{
+  const currentTime=new Date();
+  res.send(`<h1>The current time is: ${currentTime.toString()}</h1>`);
+});
+
+// start the server:
+const port = 8080;
+const serveer = app.listen(port, ()=>{
+  console.log("Waiting for requests on port %s", port);
+});
